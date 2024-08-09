5d6c02d39f6ac6538842cfd9ba1d97f4022b2230
Miguel <miguel.bezerra1@gmail.com>
2024-08-08 23:37:32 -0300

    Adiciona testes em agendaContatos

--- src/main/java/map/AgendaContatos.java
+++ src/main/java/map/AgendaContatos.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AgendaContatos {
-    //Attributes
+    // Attributes
     private Map<String, Integer> agendaContatoMap;
 
     public AgendaContatos() {
@@ -16,21 +16,38 @@ public class AgendaContatos {
     }
 
     public void removerContato(String nome) {
-        if (!agendaContatoMap.isEmpty()) {
-            agendaContatoMap.remove(nome);
-        }
+        agendaContatoMap.remove(nome);
     }
 
-    public void exibirContato() {
+    public void exibirContatos() {
         System.out.println(agendaContatoMap);
     }
 
     public Integer pesquisarPorNome(String nome) {
-        Integer numeroPorNome = null;
-        if(!agendaContatoMap.isEmpty()) {
-            numeroPorNome = agendaContatoMap.get(nome);
-        }
-        return numeroPorNome;
+        return agendaContatoMap.get(nome);
     }
 
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("João", 5665);
+        agendaContatos.adicionarContato("Carlos", 1111111);
+        agendaContatos.adicionarContato("Ana", 654987);
+        agendaContatos.adicionarContato("Maria", 1111111);
+        agendaContatos.adicionarContato("Camila", 44444);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Maria");
+        agendaContatos.exibirContatos();
+
+        String nomePesquisa = "João";
+        Integer numeroPesquisa = agendaContatos.pesquisarPorNome(nomePesquisa);
+        System.out.println("Número de telefone de " + nomePesquisa + ": " + numeroPesquisa);
+
+        String nomePesquisaNaoExistente = "Paula";
+        Integer numeroPesquisaNaoExistente = agendaContatos.pesquisarPorNome(nomePesquisaNaoExistente);
+        System.out.println("Número de telefone de " + nomePesquisaNaoExistente + ": " + numeroPesquisaNaoExistente);
+    }
 }
